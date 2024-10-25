10c1d87841d656f2c4ca5793a56f93b5df7b104e
luis-fernando-job <fernandoluis270@gmail.com>
2024-10-25 17:26:08 -0300

    rota-de-login-finalizada

--- src/index.js
+++ src/index.js
@@ -13,23 +13,22 @@ app.listen(port, () => {
 
 client.connect;
 
+// VERIFICAÇÃO DO USUARIO, VERIFICANDO SE O USUARIO EXISTE NO BANCO DE DADOS
+// ESTÁ REQUEST VEM DA PAGINA DE LOGIN
 app.post('/', async (req, res, next) => {
     const user = req.body
-    const userQuery = await client.query(`SELECT * FROM drivers WHERE email = '${user.email_login}' AND pass = '${user.password_login}'`) //seria algo aqui ?
+    const userQuery = await client.query(`SELECT * FROM drivers WHERE email = '${user.email_login}' AND pass = '${user.password_login}'`)
 
     if(userQuery.rows.length === 0) {
-        // console.log(user) teste para ver a requisição 
-        // console.log(userQuery.command) teste para ver o comando SQL(não foi possivel ver)
         return res.send('Email ou Senha incorretos!')
     }
-
     res.send('Login realizado com sucesso!')
-
 })
 
+// CADASTRANDO USUARIO NO BANCO DE DADOS
+// ESTÁ REQUISIÇÃO VEM DA PAGINA DE CADASTRO
 app.post('/cadastro', (req, res) => {
     const user = req.body
-    // console.log(user)
     const insertQuery = `INSERT INTO drivers (full_name, email, phone, pass, car_plate, car_model) VALUES ( 
         '${user.full_name}',
         '${user.email}', 
@@ -38,8 +37,6 @@ app.post('/cadastro', (req, res) => {
         '${user.car_plate}', 
         '${user.car_model}' 
         )`
-        console.log(insertQuery)
-
 
     client.query(insertQuery, (err, reuslt) => {
         if(!err) {
